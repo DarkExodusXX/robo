cbe5cf21ad885eca6cb4d97bebd8075116b7d2e5
Denis Goncharov <darkexodusxx@gmail.com>
2020-08-18 11:58:37 +0300

    Updates

---
+++ precache-manifest.4bf12e732cc6e382e4ce93375057834c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0978d3815c8be02715e5fc55fdd4e25a",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "4cf38c8163921d162759",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b1b11cd05e24ad4e76d6",
+    "url": "/robo/static/css/main.e0ff72de.chunk.css"
+  },
+  {
+    "revision": "4cf38c8163921d162759",
+    "url": "/robo/static/js/2.6b4e3719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.6b4e3719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b11cd05e24ad4e76d6",
+    "url": "/robo/static/js/main.6f1805ed.chunk.js"
+  },
+  {
+    "revision": "cd01cbebd96a4c029298",
+    "url": "/robo/static/js/runtime-main.a78c4623.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
